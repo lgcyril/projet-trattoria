0010b91d3fd3de85f1d8ad085396f87fbbe7d2bc
Cyril Luong <lgcyriltt@gmail.com>
2023-02-07 17:43:33 +0100

    orderitem

--- src/components/OrderItem.js
+++ src/components/OrderItem.js
@@ -1,9 +1,17 @@
 import '../styles/OrderItem.css'
+import VoteScale from './VoteScale'
 
-function OrderItem({children}) {
-    return  <div className='tra-orderitem'>{children}
-        
-         </div>
+// water=vote, light=price, sclae=vote
+
+function OrderItem({id, cover, name, vote, price }) {
+    return  	(<li key={id} className='tra-pasta-item'>
+    <img className='tra-pasta-item-cover' src={cover} alt={`${name} cover`} />
+    {name}
+    <div>
+        <VoteScale voteType='vote' voteValue={vote} />
+        <VoteScale voteType='price' voteValue={price} />
+    </div>
+</li>)
   }
 
   export default OrderItem
